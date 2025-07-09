df65795151619f447079fb9e5cd14dbb3d239faa
TheRealLorenz <lollobellina@gmail.com>
2025-07-09 15:44:13 +0200

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  title: string;
+  url?: string;
+  primary?: boolean;
+};
+
+export default function Button({ title, url, primary }: Props) {
+  return (
+    <a href={url}>
+      <div
+        className={`${primary ? "rocket-gradient" : "border-primary border-4"} w-fit py-2 px-16 rounded-md text-white text-2xl md:text-4xl font-bold flex items-center font-condensed hover:scale-110 transition-scale ease-in-out duration-200`}
+      >
+        {title}
+      </div>
+    </a>
+  );
+}
