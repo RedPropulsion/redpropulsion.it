5ab0c1acb1ceffec5204bc0705858f39861cd10a
TheRealLorenz <lollobellina@gmail.com>
2025-07-09 23:36:45 +0200

    Add departments page

---
+++ src/app/departments/page.tsx
@@ -0,0 +1,32 @@
+import Content from "@/content/departments_page.json";
+import Navbar from "@/components/Navbar";
+import Block, { validateBlockContent } from "@/components/Block";
+import UnderConstruction from "@/components/UnderConstruction";
+
+export function generateMetadata() {
+  return {
+    title: Content.title,
+    description: Content.description,
+  };
+}
+
+export default function Page() {
+  if (Content.sections.length === 0) {
+    return UnderConstruction();
+  }
+
+  return (
+    <>
+      <Navbar alwaysShow={true} />
+      <div
+        style={{
+          height: "100px",
+        }}
+      ></div>
+      {Content.sections.map((content, i) => {
+        validateBlockContent(content);
+        return <Block content={content} key={i} />;
+      })}
+    </>
+  );
+}
