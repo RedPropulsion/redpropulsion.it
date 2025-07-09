a016fc406211ec2b9143e659f3a0ef25d9876893
TheRealLorenz <lollobellina@gmail.com>
2025-07-09 19:34:00 +0200

    Update sponsors section

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -2,16 +2,14 @@ import Content from "@/content/sponsors.json";
 
 export default function Sponsors() {
   return Content.sponsors.length > 0 ? (
-    <div className="bg-background-dark">
+    <div className="bg-background-dark px-4">
       <h2 className="font-condensed text-center text-gradient text-5xl font-bold">
         I nostri sponsor
       </h2>
-      <div className="flex justify-center py-16 flex-wrap mx-auto gap-8">
+      <div className="flex justify-center items-center  py-16 flex-wrap mx-auto gap-8">
         {Content.sponsors.map((item, i) => (
           <a key={i} href={item.url}>
-            <div className="flex flex-col">
-              <img src={item.image} alt={item.name} className="h-20" />
-            </div>
+            <img src={item.image} alt={item.name} className="w-40" />
           </a>
         ))}
       </div>
