8c9d32d29ab1895bfbd49a1441ee4c2744cbdc50
TheRealLorenz <lollobellina@gmail.com>
2025-07-09 17:08:24 +0200

    Better navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,84 @@
+"use client";
+
 import ShowOnScroll from "@/components/ShowOnScroll";
 
 import Content from "@/content/navbar.json";
+import { AlignJustify } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 type Props = {
   alwaysShow?: boolean;
 };
 
 export default function Navbar({ alwaysShow }: Props) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <ShowOnScroll active={!alwaysShow}>
-      <div className="fixed w-full p-4 bg-background-dark z-100 flex">
-        <Link href="/">
-          <h1 className="text-white font-bold mr-4 text-3xl hover:underline">
-            {Content.title}
-          </h1>
-        </Link>
-        <div className="flex align-center gap-4 my-auto">
+      {/* <div className="fixed w-full p-4 bg-background-dark z-100 flex flex-col md:flex-row"> */}
+      {/*   <Link href="/"> */}
+      {/*     <h1 className="text-white font-condensed font-bold mr-8 text-3xl hover:underline"> */}
+      {/*       {Content.title} */}
+      {/*     </h1> */}
+      {/*   </Link> */}
+      {/*   <AlignJustify */}
+      {/*     className="text-primary ml-auto my-auto w-[30px] h-[30px] md:hidden absolute right-4" */}
+      {/*     onClick={() => setMenuOpen(!menuOpen)} */}
+      {/*   /> */}
+      {/*   <div */}
+      {/*     className={`align-center gap-8 my-auto flex-col md:flex-row flex transition-transform ease-in-out duration-300 transform ${menuOpen ? "translate-y-0" : "-translate-y-full"} -z-1`} */}
+      {/*   > */}
+      {/*     {Content.links.map((item, i) => ( */}
+      {/*       <a */}
+      {/*         key={i} */}
+      {/*         href={item.link} */}
+      {/*         className="text-foreground-dim hover:underline font-condensed text-2xl" */}
+      {/*       > */}
+      {/*         {item.title} */}
+      {/*       </a> */}
+      {/*     ))} */}
+      {/*   </div> */}
+      {/* </div> */}
+      <div className="fixed w-full p-4 bg-background-dark z-20 flex items-center justify-between">
+        {/* Contenitore per Titolo e Link Desktop */}
+        <div className="flex items-center gap-8">
+          <Link href="/">
+            <h1 className="text-white font-condensed font-bold text-3xl hover:underline">
+              {Content.title}
+            </h1>
+          </Link>
+          {/* Link per Desktop: visibili solo da 'md' in su */}
+          <div className="hidden md:flex gap-8 my-auto">
+            {Content.links.map((item, i) => (
+              <a
+                key={i}
+                href={item.link}
+                className="text-foreground-dim hover:underline font-condensed text-2xl"
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+        </div>
+
+        {/* Hamburger Icon: visibile solo fino a 'md' */}
+        <AlignJustify
+          className="text-primary w-[30px] h-[30px] md:hidden cursor-pointer"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+
+        {/* Menu a Tendina per Mobile */}
+        <div
+          className={`absolute top-full left-0 w-full bg-background-dark px-4 pb-4 flex flex-col items-center gap-4 md:hidden transition-transform duration-400 ease-in-out transform -z-1 border-b-2 border-primary ${menuOpen ? "translate-y-0" : "-translate-y-3/2"}
+        `}
+        >
           {Content.links.map((item, i) => (
             <a
               key={i}
               href={item.link}
-              className="text-foreground-dim hover:underline"
+              className="text-foreground-dim hover:underline font-condensed text-2xl"
+              onClick={() => setMenuOpen(false)} // Optional: chiude il menu al click
             >
               {item.title}
             </a>
