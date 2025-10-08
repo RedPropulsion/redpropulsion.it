1bcf796e8b12fc2f70c1b46b73501a5353bbbd5a
TheRealLorenz <lollobellina@gmail.com>
2025-10-08 16:33:24 +0200

    fix: explore button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,30 @@
+"use client";
+
 export type ButtonProps = {
   title: string;
   url?: string;
   primary?: boolean;
 };
 
+function scrollDown() {
+  window.scrollBy({
+    top: window.innerHeight * 0.9,
+    behavior: "smooth",
+  });
+}
+
 export default function Button({ title, url, primary }: ButtonProps) {
+  if (url === "scroll") {
+    return (
+      <div
+        className={`${primary ? "rocket-gradient" : "border-primary border-4"} w-full py-2 px-16 rounded-md text-white text-2xl md:text-4xl font-bold flex items-center font-condensed hover:scale-110 transition-scale ease-in-out duration-200 justify-center`}
+        onClick={scrollDown}
+      >
+        {title}
+      </div>
+    );
+  }
+
   return (
     <a href={url}>
       <div
